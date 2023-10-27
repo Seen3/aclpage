b63ffad7ed2412ac8dde40f133f9e75d17be3593
Seen3 <littlefinger331@gmail.com>
2023-10-27 11:28:12 +0530

    Added a sample frontend for flow rules

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ACL_Comp from './ACL_Comp';
+import FlowControlApp from './Flowapp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ACL_Comp/>
+    <FlowControlApp/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
