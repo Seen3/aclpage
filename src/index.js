a6cb697135ff3dd651801e626d579e3bac0e9849
Seen3 <littlefinger331@gmail.com>
2023-11-11 11:55:04 +0530

    added routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import ACL_Comp from './ACL_Comp';
 import FlowControlApp from './Flowapp';
@@ -8,6 +9,10 @@ import MiniG from './mininet';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MiniG/>
+    <Router>
+      <Route path="/acl" component={ACL_Comp} />
+      <Route path="/flow" component={FlowControlApp} />
+      <Route path="/minig" component={MiniG} />
+    </Router>
   </React.StrictMode>
 );
