6be71cdf3999166120af14e2d7a85004658c1470
Seen3 <littlefinger331@gmail.com>
2023-11-13 00:04:49 +0530

    KKK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import ACL_Comp from './ACL_Comp';
 import FlowControlApp from './Flowapp';
@@ -16,16 +16,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <Route exact path="/" element={<SignIn />} />
-          <Route path="/Signup" element={<SignUp />} />
-          <Route
-            path="/Home"
-            element={email ? <Home /> : <Navigate to="/" />}
-          />
-
-      <Route path="/acl" component={ACL_Comp} />
-      <Route path="/flow" component={FlowControlApp} />
-      <Route path="/minig" component={MiniG} />
+      <Routes>
+        <Route exact path="/" element={<SignIn />} />
+        <Route path="/Signup" element={<SignUp />} />
+        <Route
+          path="/Home"
+          element={email ? <Home /> : <Navigate to="/" />}
+        />
+        <Route path="/acl" element={<ACL_Comp />} />
+        <Route path="/flow" element={<FlowControlApp />} />
+        <Route path="/minig" element={<MiniG />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
