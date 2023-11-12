4ddad16004e046a149f659c711649f2e9fc1346c
Seen3 <littlefinger331@gmail.com>
2023-11-12 23:34:05 +0530

    Added Hrishikesh blockchain part

--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import ACL_Comp from './ACL_Comp';
 import FlowControlApp from './Flowapp';
 import MiniG from './mininet';
 
+
+import SignIn from "./Screens/Signin";
+import SignUp from "./Screens/Signup";
+import Home from "./Screens/Home";
+
+const email = localStorage.getItem("email");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+    <Route exact path="/" element={<SignIn />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route
+            path="/Home"
+            element={email ? <Home /> : <Navigate to="/" />}
+          />
+
       <Route path="/acl" component={ACL_Comp} />
       <Route path="/flow" component={FlowControlApp} />
       <Route path="/minig" component={MiniG} />
